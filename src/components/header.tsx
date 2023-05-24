55e14dd26fa2796fa9fe5a2e34e223874729e5d5
Enrique Capellan <enriqcapellan@gmail.com>
2023-05-23 21:53:40 -0400

    add user avatar (#1)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,43 @@
 import styled from "styled-components";
 
 function Header() {
-  return (
-    <Wrapper>
-      <div className="container--log">
-        <img src="" alt="user" />
-      </div>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<div className="container--log">
+				<img
+					src="https://e7.pngegg.com/pngimages/348/800/png-clipart-man-wearing-blue-shirt-illustration-computer-icons-avatar-user-login-avatar-blue-child.png"
+					alt="user"
+				/>
+			</div>
+		</Wrapper>
+	);
 }
 
 const Wrapper = styled.header`
-  grid-area: header;
-  margin: 0;
-  box-sizing: border-box;
-  display: grid;
-  width: 100%;
-  height: 100%;
-  nav {
-    display: grid;
-    grid-template: "logo log" 1fr/1fr 150px;
-    box-sizing: border-box;
+	grid-area: header;
+	margin: 0;
+	box-sizing: border-box;
+	display: grid;
+	width: 100%;
+	height: 100%;
+	nav {
+		display: grid;
+		grid-template: "logo log" 1fr/1fr 150px;
+		box-sizing: border-box;
 
-    button {
-      width: 100%;
-      height: 100%;
-    }
+		button {
+			width: 100%;
+			height: 100%;
+		}
 
-    .container--log {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      grid-area: log;
-      padding: 22px;
-    }
-  }
+		.container--log {
+			display: flex;
+			align-items: center;
+			justify-content: flex-end;
+			grid-area: log;
+			padding: 22px;
+		}
+	}
 `;
 
 export default Header;
