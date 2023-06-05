5229bf724a9962ad7c266e1f65e3c735a0fd7990
Andreidy Sanchez <andry.sanp@gmail.com>
2023-06-05 16:47:47 -0400

    css login component

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -7,6 +7,7 @@ import Login from "../pages/login";
 import Home from "../pages/home";
 import Reporte from "../pages/reportes";
 import Configuration from "../pages/configuration";
+import { createGlobalStyle } from "styled-components";
 
 const router = createBrowserRouter([
   {
@@ -40,7 +41,24 @@ const router = createBrowserRouter([
 ]);
 
 function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />;
+    </>
+  );
 }
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: "Poppins-Regular";
+    src: local("Poppins-Regular") url('./fonst/Poppins/Poppins-Regular.ttf') format('truetype');
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export default Router;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,30 +1,142 @@
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useSupabase } from "../../hooks/useSupabase";
+import { customVariables } from "./constants";
 
 function Login() {
+  const { session, supabase } = useSupabase();
   const navigate = useNavigate();
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    navigate("/");
-  };
+  if (session) navigate("/");
+  document.querySelector("a")?.classList.add("linkLabel");
+  document.querySelector("#email")?.classList.add("user-icon");
+  document.querySelector("#password")?.classList.add("password-icon");
 
   return (
     <Wrapper>
-      <h2>Iniciar Sección</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Usuario</label>
-        <input id="username" type="text" placeholder="Jocelin Sanchez" />
-        <label htmlFor="userPassword">Contraseña</label>
-        <input id="userPassword" type="password" placeholder="******" />
-        <input type="submit" />
-      </form>
+      <div className="logo-container">
+        <img className="AD-logo" src="assets/images/LOGO.png" alt="Logo" />
+      </div>
+      <div className="log-container">
+        <div className="log">
+          <h1>INICIO</h1>
+
+          <Auth
+            providers={[]}
+            supabaseClient={supabase}
+            localization={{
+              variables: customVariables,
+            }}
+            appearance={{
+              theme: ThemeSupa,
+              className: { button: "button", input: "input" },
+            }}
+          />
+        </div>
+      </div>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  border: 1px solid black;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  gap: 250px;
+
+  .linkLabel {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 30px;
+    letter-spacing: -0.005em;
+    font-style: italic;
+    text-decoration: none;
+    font-family: "Poppins";
+  }
+
+  img {
+    /* width: 398px; */
+    border-radius: 10px;
+  }
+
+  .log-container {
+    width: 330px;
+    height: 472.875px;
+    background: #085690;
+    box-shadow: -15px 0px 24px rgba(0, 0, 0, 0.25);
+    border-radius: 40px;
+    padding: 85px 45px;
+  }
+  .log {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.14);
+    border-radius: 40px;
+    backdrop-filter: blur(5.5px);
+    width: 100%;
+    height: 100%;
+  }
+
+  h1 {
+    font-family: "Poppins-Bold", "Poppins";
+    font-style: normal;
+    font-size: 32px;
+    line-height: 48px;
+    letter-spacing: 0.175em;
+    color: #ffffff;
+    margin-top: 65px;
+    margin-bottom: 0px;
+  }
+  .input {
+    width: 255px;
+    height: 50px;
+    padding: 10px;
+    background-color: rgba(255, 255, 255, 0.52);
+    border-radius: 20px;
+  }
+  form div {
+    align-items: center;
+  }
+  .button {
+    background-color: #4e8fff;
+    border-color: #4e8fff;
+    border-radius: 10px;
+    width: 200px;
+    font-family: Poppins;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 30px;
+    letter-spacing: 0.175em;
+    color: #ffffff;
+  }
+  .button:hover {
+    background-color: #82aefad0;
+  }
+
+  .input,
+  .input::placeholder {
+    font-family: "Poppins";
+    font-style: italic;
+    font-size: 16px;
+    line-height: 36px;
+    letter-spacing: -0.005em;
+    color: #dadada;
+  }
+  .input {
+    color: #ffffff;
+  }
+  .user-icon {
+    background: url(assets/images/user-icon.svg) no-repeat 10px 10px;
+    padding-left: 43px;
+  }
+  .password-icon {
+    background: url(assets/images/pass-icon.svg) no-repeat 10px 10px;
+    padding-left: 40px;
+  }
 `;
 
 export default Login;
