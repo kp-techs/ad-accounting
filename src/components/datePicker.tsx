be99f79a1b3da2c737d614e40c15133cbd063e92
Andreidy Sanchez <andry.sanp@gmail.com>
2023-06-05 19:09:04 -0400

    dates layout

---
+++ src/components/datePicker.tsx
@@ -0,0 +1,23 @@
+import { FieldProps } from "formik";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
+registerLocale("es", es);
+
+type Props = FieldProps<any>;
+
+function PickDate({ form, field }: Props) {
+  console.log(field.value);
+  return (
+    <DatePicker
+      showTimeSelect
+      selected={field.value}
+      onChange={(value) => {
+        form.setFieldValue(field.name, value);
+      }}
+    />
+  );
+}
+
+export default PickDate;
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,15 +11,24 @@ export function generateConcept({ type, ...income }: TableIncome) {
     : income.incomeTypes.name;
 }
 
+export function captalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export function formatDate(date: string | null) {
   moment.locale("es");
   const momentDate = moment(date);
   const daysPassed = moment().diff(moment(date, "YYYYMMDD"), "days");
   const yearsPassed = moment().diff(moment(date, "YYYYMMDD"), "year");
 
-  if (daysPassed < 7) {
+  if (daysPassed < 0) {
     const newDate = momentDate.fromNow();
-    return newDate.charAt(0).toUpperCase() + newDate.slice(1);
+    return captalize(newDate);
+  } else if (daysPassed === 0) {
+    return "Hoy";
+  } else if (daysPassed < 7) {
+    const day = momentDate.format("dddd");
+    return captalize(day);
   } else if (yearsPassed === 0) {
     return momentDate.format("D [de] MMMM");
   } else {
