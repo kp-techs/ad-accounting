9e41f12a2d6262290d50936f259f479b3b057205
Oscar <eliasromerohd@gmail.com>
2023-06-08 23:54:57 -0400

    signUpForm integrated to supabase

--- src/hooks/useSupabase.tsx
+++ src/hooks/useSupabase.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { TableIncome } from "../types/models";
 import { Database } from "../types/supabase";
 
-const supabase = createClient<Database>(
+export const supabase = createClient<Database>(
   process.env.REACT_APP_SUPABASE_URL || "",
   process.env.REACT_APP_SUPABASE_KEY || ""
 );
