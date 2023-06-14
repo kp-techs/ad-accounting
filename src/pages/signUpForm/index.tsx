d706e042c001b74f01af2e8d29a1df2294cc923a
Oscar <eliasromerohd@gmail.com>
2023-06-13 20:13:18 -0400

    invitation form

--- src/pages/signUpForm/index.tsx
+++ src/pages/signUpForm/index.tsx
@@ -1,68 +1,39 @@
+import { useState } from 'react'
 import styled from "styled-components";
 import { useFormik } from 'formik';
+import { supabase } from "../../hooks/useSupabase";
 
 const SignUpForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
 
-    const { handleSubmit, handleChange } = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            role: '',
-            password: '',
+  const handleLogin = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
 
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
+  }
 
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
-
-    return (
-        <Wrapper>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Nombre</label>
-                <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="Jhonathan Ortiz"
-                    onChange={handleChange} />
-
-                <label htmlFor="correo">Correo</label>
-                <input
-                    id="correo"
-                    type="email"
-                    name="email"
-                    placeholder="correo@gmail.com"
-                    onChange={handleChange} />
-
-                <label htmlFor="rol">Rol</label>
-                <select id="role" name="role" onChange={handleChange}>
-                    <option value="Administrador">Administrador</option>
-                    <option value="Usuario">Usuario</option>
-                </select>
-
-                <label htmlFor="password">Contraseña</label>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="********"
-                    onChange={handleChange} />
-
-                <input type="submit" value="Registrar" />
-            </form>
-        </Wrapper>
-    );
+  return (
+    <div>
+      <h1>Registrar usuario</h1>
+      <form className="form-widget" onSubmit={handleLogin}>
+        <div>
+          <input
+            type="email"
+            placeholder="Correo Electronico"
+            value={email}
+            required={true}
+            onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <button disabled={loading}>
+            {loading ? <span>Loading</span> : <span>Enviar enlace de registro</span>}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
-
-const Wrapper = styled.section`
-  box-sizing: border-box;
-  display: grid;
-  margin: 20px;
-  border-radius: 8px;
-  border: 1px solid #e8eaed5c;
-  background-color: #ffffffc0;
-`;
 
 export default SignUpForm;
