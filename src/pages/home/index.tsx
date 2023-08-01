0bf9f3ef009d1b715a4870f82cd129a8a1b9bcd5
Enrique Capellan <enriqcapellan@gmail.com>
2023-07-31 21:42:37 -0400

    starting home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,22 @@ function Home() {
   return (
     <Wrapper>
       <section className="resumen">
-
+        <div>
+          <p className="title">INGRESOS</p>  
+          <p></p>
+        </div>
+        <div>
+          <p className="title">EGRESOS</p>  
+          <p></p>
+        </div>
+        <div>
+          <p className="title">PRESTAMOS</p>  
+          <p></p>
+        </div>
+        <div>
+          <p className="title">BALANCE</p>  
+          <p></p>
+        </div>
       </section>
     </Wrapper>
   );
